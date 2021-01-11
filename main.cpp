c6edfe6d3a2f80b8a1faf551676d1cd4597af349
RadostinaYosheva <r.yosheva@gmail.com>
2021-01-11 14:34:18 +0200

    Reove comments and memory function

--- main.cpp
+++ main.cpp
@@ -1,17 +1,5 @@
 #include "engine.h"
-#include <sys/time.h>
-#include <sys/resource.h>
 
-long getMemoryUsage()
-{
-    rusage myUsage;
-
-    getrusage(RUSAGE_SELF, &myUsage);
-
-    return myUsage.ru_maxrss;
-}
-
-// TODO: Make an engine class and in main have one line "engine.run();"
 int main()
 {
     Engine engine;
